47d5262eee16b71f727b28b46d11af64c1d1971c
Katrina Ezis <katrinaezis@gmail.com>
2017-04-21 02:08:27 -0700

    Starter code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBS-_ltv2Ba09OWG5xlr-8jZvEXfexnnJk",
+    authDomain: "pulo-934f2.firebaseapp.com",
+    databaseURL: "https://pulo-934f2.firebaseio.com",
+    projectId: "pulo-934f2",
+    storageBucket: "pulo-934f2.appspot.com",
+    messagingSenderId: "97969479543"
+};
+firebase.initializeApp(config);
