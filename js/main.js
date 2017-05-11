e175c02f615f1be903601c29ab8597ae463143cf
Katrina Ezis <katrinaezis@gmail.com>
2017-05-11 14:11:07 -0700

    dashboardish

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@
 
 // Initializes PeerIo.
 function PeerIo() {
+
+  
   this.checkSetup();
 
   // Shortcuts to DOM Elements.
@@ -321,5 +323,21 @@ PeerIo.prototype.checkSetup = function() {
 window.onload = function() {
     
   window.peerio = new PeerIo();
+  var bar = new ProgressBar.Circle(container, {
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  duration: 1400,
+  easing: 'bounce',
+  strokeWidth: 6,
+  from: {color: '#FFEA82', a:0},
+  to: {color: '#ED6A5A', a:1},
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+  }
+});
 };
+
+ 
 
